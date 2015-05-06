f5c12b4b3a43166618261bc70a09dbebdaa34240
Fernando Giorgetti <fgiorgetti@gmail.com>
2015-05-06 01:38:35 -0300

    Added a few more properties to the TileMap class.

--- src/main/java/br/com/giorgetti/games/squareplatform/gamestate/levels/Level1.java
+++ src/main/java/br/com/giorgetti/games/squareplatform/gamestate/levels/Level1.java
@@ -2,11 +2,16 @@ package br.com.giorgetti.games.squareplatform.gamestate.levels;
 
 import br.com.giorgetti.games.squareplatform.gamestate.GameState;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 /**
  * Created by fgiorgetti on 5/1/15.
  */
 public class Level1 extends LevelStateManager {
+
+    public void draw(Graphics2D g) {
+
+    }
 
 }
--- src/main/java/br/com/giorgetti/games/squareplatform/tiles/TileMap.java
+++ src/main/java/br/com/giorgetti/games/squareplatform/tiles/TileMap.java
@@ -1,5 +1,7 @@
 package br.com.giorgetti.games.squareplatform.tiles;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,6 +30,9 @@ public class TileMap {
     private static final int POS_SPRITE_Y = 0;
     private static final int POS_SPRITE_CLASS = 0;
 
+    private static final int POS_BG_SPEED = 0;
+    private static final int POS_BG_PATH  = 1;
+
     /**
      * Stores the tileSet with all images that can be referenced to
      * by the tile map. The Integer key is associated with the tileSetIdMap,
@@ -37,34 +42,48 @@ public class TileMap {
      */
     private Map<Integer, TileSet> tileSetMap;
     private Map<String, Integer> tileSetIdMap;
-    private String[][] map;
+
+    // Dimensions
+    private int rows, cols, height, width;
+
+    // Backgrounds
+    private List<String[]> backgrounds;
+
+    // Player initial position
+    private int playerX, playerY;
+
+    // Stores identification for each sprite on the map
     private String[] sprites;
+    // Tile map, defining all tiles in the map
+    private Map<Integer,Map<Integer,String[]>> map = new HashMap<Integer, Map<Integer, String[]>>();
 
     /**
      * Loads the tile map from the given path. First attempts to
      * read as a resource and if not possible, then tries as a regular file.
      *
      * Initial model is supposed to be as:
+     *
      * ROWS=100 # Number of rows based on the tilemap height
      * COLS=500 # Number of columns based on the tilemap width
      * WIDTH=32 # Width in pixels for the tiles
      * HEIGHT=32 # Height in pixels for the tiles
-     * 1,Terrain,tileset/terrain.jpg # 1 = tile set id, Terrain = tile set name, tileset/terrain.jpg = image path
-     * 2,Sprites,tileset/sprites.jpg
-     * PLAYER=50,50 # Player initial position on the map (x,y)
+     * BG=10,background/sky.jpg 25,background/city.jpg# 10 = percent of player speed
+     * TS=1,Terrain,tileset/terrain.jpg 2,Sprites,tileset/sprites.jpg # 1 = tile set id, Terrain = tile set name, tileset/terrain.jpg = image path
+     * PLAYER_POS=50,50 # Player initial position on the map (x,y)
      * SPRITES=100,50,Enemy1 200,30,Enemy2 # 100 = x, 50 = y, Enemy1 = Java Class Representing the Sprite
-     * 1,1,0 1,1,0 1,1,0 # First row of the map. 1 = Tile set ID, 1 = Tile Position ID in the tileset, 0 = tile type
-     * 1,1,0 1,1,0 1,1,0 # Second row of the map. 1 = Tile set ID, 1 = Tile Position ID in the tileset, 0 = tile type
-     * TBD
+     * [0] 0,1,1,0 1,1,1,0 2,1,1,0 # [0] = First row of the map, 0 = First column, 1 = Tile set ID, 1 = Tile Position ID in the tileset, 0 = tile type
+     * [2] 0,1,1,0 1,1,1,0 2,1,1,0 # [2] = Third row of the map, 0 = First column, 1 = Tile set ID, 1 = Tile Position ID in the tileset, 0 = tile type
      *
      * @param tileMapPath
      */
     public void loadTileMap(String tileMapPath) {
 
+        // Opening the tile map file
+
     }
 
     private String[] getTileInfo(String tileMapInfo) {
-        return tileInfo.split(SEPARATOR);
+        return tileMapInfo.split(PROP_SEPARATOR);
     }
 
 }
