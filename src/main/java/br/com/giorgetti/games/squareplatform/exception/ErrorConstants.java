985921c39c370c66c61efe2d45e98e44916811cd
Fernando Giorgetti <fgiorgetti@gmail.com>
2015-05-08 01:25:31 -0300

    Implemented initial non-tested version of the TileMap loader.

---
+++ src/main/java/br/com/giorgetti/games/squareplatform/exception/ErrorConstants.java
@@ -0,0 +1,16 @@
+package br.com.giorgetti.games.squareplatform.exception;
+
+/**
+ * Stores constants to represent the available error codes.
+ *
+ * Created by fgiorgetti on 5/7/15.
+ */
+public class ErrorConstants {
+
+    /**
+     * Tile Map related errors from 100 to 199
+     */
+    public static final int INVALID_TILE_MAP_PATH       = 100;
+    public static final int IO_ERROR_READING_TILE_MAP   = 101;
+
+}
