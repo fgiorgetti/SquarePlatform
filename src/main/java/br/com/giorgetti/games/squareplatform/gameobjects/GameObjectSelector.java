aa51f6a03bc36456fd7a36a9314f67bfcefe3f85
Fernando Giorgetti <fgiorgetti@gmail.com>
2017-07-14 19:06:36 -0300

    First pre-working implementation of a Sprite in the Game Editor.

---
+++ src/main/java/br/com/giorgetti/games/squareplatform/gameobjects/GameObjectSelector.java
@@ -0,0 +1,63 @@
+package br.com.giorgetti.games.squareplatform.gameobjects;
+
+import br.com.giorgetti.games.squareplatform.gameobjects.interaction.Button;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Provides a list of all available GameObjects that can be
+ * used through Map Editor.
+ *
+ * Created by fgiorgetti on 14/07/17.
+ */
+public class GameObjectSelector {
+
+    private static final ArrayList<String> gameObjectsList = new ArrayList<>();
+    private static final Map<String, Class<? extends Sprite>> gameObjectsMap = new TreeMap<>();
+
+    static {
+        gameObjectsMap.put("Button", Button.class);
+        for ( String key : gameObjectsMap.keySet() ) {
+            gameObjectsList.add(key);
+        }
+    }
+
+    public static String getObjectClassName(String objectName) {
+        return gameObjectsMap.get(objectName).getName();
+    }
+
+    public static String getPreviousGameObject(String previous) {
+
+        int idx = 0;
+        if ( gameObjectsList.contains(previous) ) {
+            idx = gameObjectsList.indexOf(previous) - 1;
+        }
+
+        // Cycle back to first
+        if ( idx < 0 ) {
+            idx = gameObjectsList.size() - 1;
+        }
+
+        return gameObjectsList.get(idx);
+
+    }
+    public static String getNextGameObject(String previous) {
+
+        int idx = 0;
+        if ( gameObjectsList.contains(previous) ) {
+            idx = gameObjectsList.indexOf(previous) + 1;
+        }
+
+        // Cycle back to first
+        if ( idx == gameObjectsList.size() ) {
+            idx = 0;
+        }
+
+        return gameObjectsList.get(idx);
+
+    }
+
+
+}
